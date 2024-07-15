992af12589126afabe410cc0c1c12fca318ef457
SZKrisztian <szaloczy6@gmail.com>
2024-07-15 21:50:26 +0200

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+//MODULES
+const names = require('./firstmodule');
+const sayHi = require('./secondmodule');
+const data = require('./alternative_exp');
+
+require('./mind-granade')
+/* 
+sayHi(names.john)
+sayHi(names.peter) */
---
+++ firstmodule.js
@@ -0,0 +1,7 @@
+//local
+const secret = 'SUPER SECRET'
+// share
+const john = "jhon"
+const peter = "peter"
+
+module.exports = {john,peter}
