300c08b2db8a6c58f6b96c32d979fcd43616c157
SZKrisztian <107873757+szaloczy@users.noreply.github.com>
2024-08-07 14:10:04 +0200

    CRUD-with-express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,31 +1,47 @@
 const express = require('express');
-const logger = require('./logger');
-const authorize = require('./authorize');
 const app = express();
+let { people } = require('./data');
 
-app.use([logger, authorize]);
+//static assets
+app.use(express.static('./methods-public'))
 
-//req => middleware => res
+//parse form data
+app.use(express.urlencoded({extended:false}));
 
- 
-app.get('/', (req,res)=> {
+//parse json
+app.use(express.json())
 
-   res.send('Home');
+app.get('/api/people', (req,res) => {
+   res.status(200).json({success:true, data:people} );
 });
 
-app.get('/about', (req,res) => {
-   res.send('About');
+app.post('/api/people', (req,res) => {
+   const {name} = req.body;
+
+   if(!name) {
+      return res.status(400).json({success:false, msg:'please provide name value'});
+   }
+      res.status(201).json({success:true,person:name})
 });
 
-app.get('/api/v1/products', (req,res) => {
-   res.send('products');
+app.post('/api/postman/people',(req,res) => {
+   const {name} = req.body;
+   if(!name){
+      return res
+      .status(400)
+      .json({success:false, msg:'please provide name value'});
+   }
+   res.status(201).json({success:true,data:people})
 });
 
-app.get('/api/v1/items',(req,res) => {
-   console.log(req.user);
-   res.send('items')
-})
+app.post('/login', (req,res) => {
+   const {name} = req.body;
+   if(name) {
+      return res.status(200).send(`Welcome ${name}`);
+   }
+   res.status(401).send('Please provide a credentials');
+});
 
-app.listen(5000, () => {
-   console.log("Server listening on port 5000...");
+app.listen(5000,() => {
+   console.log('Server istening on port 5000...');
 });
