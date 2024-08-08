3879924ba671fa0228ac338910cda284b6c1ed44
Szaloczy <szaloczy6@gmail.com>
2024-08-08 15:20:02 +0200

    mvc-structure

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-let { people } = require('./data');
+const people = require('./routes/people');
+const auth = require('./routes/auth');
 
 //static assets
 app.use(express.static('./methods-public'))
@@ -9,38 +10,10 @@ app.use(express.static('./methods-public'))
 app.use(express.urlencoded({extended:false}));
 
 //parse json
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/people', (req,res) => {
-   res.status(200).json({success:true, data:people} );
-});
-
-app.post('/api/people', (req,res) => {
-   const {name} = req.body;
-
-   if(!name) {
-      return res.status(400).json({success:false, msg:'please provide name value'});
-   }
-      res.status(201).json({success:true,person:name})
-});
-
-app.post('/api/postman/people',(req,res) => {
-   const {name} = req.body;
-   if(!name){
-      return res
-      .status(400)
-      .json({success:false, msg:'please provide name value'});
-   }
-   res.status(201).json({success:true,data:people})
-});
-
-app.post('/login', (req,res) => {
-   const {name} = req.body;
-   if(name) {
-      return res.status(200).send(`Welcome ${name}`);
-   }
-   res.status(401).send('Please provide a credentials');
-});
+app.use('/api/people', people);
+app.use('/login',auth);
 
 app.listen(5000,() => {
    console.log('Server istening on port 5000...');
